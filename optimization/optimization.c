19549c497da9e68311003b5a377d61adff8ce5f2
PRATHAM-SPS <pratham.201343101@vcet.edu.in>
2023-04-25 21:55:44 +0530

    naya optimization

--- optimization/optimization.c
+++ optimization/optimization.c
@@ -1,54 +1,131 @@
+#include <stdio.h>
+#include <string.h>
+struct op {
+char l;
+   char r[20];
+} op[10], pr[10];
+int main() {
+   int a, i, k, j, n, z = 0, m, q;
+   char *p, *l;
+   char temp, t;
+   char *tem;
+   printf("Enter the Number of Values:");
+   scanf("%d", &n);
+   for (i = 0; i < n; i++)
+   {
+       printf("left: ");
+       scanf(" %c", &op[i].l);
 
-1. Code Motion or Frequency Reduction :
+     printf("right: ");
+    scanf(" %s", &op[i].r);
+}
+printf("Intermediate Code\n");
+for (i = 0; i < n; i++)
+{
+    printf("%c=", op[i].l);
+    printf("%s\n", op[i].r);
+}
+for (i = 0; i < n - 1; i++)
+{
+    temp = op[i].l;
+    for (j = 0; j < n; j++)
+    {
+        p = strchr(op[j].r, temp);
+        if (p)
+        {
+            pr[z].l = op[i].l;
+            strcpy(pr[z].r, op[i].r);
+            z++;
+} }
+}
+pr[z].l = op[n - 1].l;
+strcpy(pr[z].r, op[n - 1].r);
+z++;
+printf("\nAfter Dead Code Elimination\n");
+for (k = 0; k < z; k++)
+{
+    printf("%c\t=", pr[k].l);
+    printf("%s\n", pr[k].r);
+}
+for (m = 0; m < z; m++)
+{
+    tem = pr[m].r;
+    for (j = m + 1; j < z; j++)
  
-a = 200;
-while(a>0)
-{
-	b = x + y;
-	if (a % b == 0)
-	printf(“%d”, a);
+     {
+        p = strstr(tem, pr[j].r);
+        if (p)
+        {
+            t = pr[j].l;
+            pr[j].l = pr[m].l;
+            for (i = 0; i < z; i++)
+            {
+                l = strchr(pr[i].r, t);
+                if (l)
+                {
+                    a = l - pr[i].r;
+                    printf("pos: %d\n", a);
+                    pr[i].r[a] = pr[m].l;
+} }
+} }
 }
-
-
-//This code can be further optimized as
-a = 200;
-b = x + y;
-while(a>0)
+printf("Eliminate Common Expression\n");
+for (i = 0; i < z; i++)
 {
-	if (a % b == 0}
-	printf(“%d”, a);
+    printf("%c\t=", pr[i].l);
+    printf("%s\n", pr[i].r);
 }
-
-2. Loop Jamming :
-
- // Before loop jamming
-for(int k=0;k<10;k++)
+for (i = 0; i < z; i++)
 {
-x = k*2;
+    for (j = i + 1; j < z; j++)
+    {
+        q = strcmp(pr[i].r, pr[j].r);
+        if ((pr[i].l == pr[j].l) && !q)
+        {
+            pr[i].l = '\0';
+        }
+} }
+ 
+    printf("Optimized Code\n");
+   for (i = 0; i < z; i++)
+   {
+       if (pr[i].l != '\0')
+       {
+           printf("%c=", pr[i].l);
+           printf("%s\n", pr[i].r);
+       }
 }
-
-for(int k=0;k<10;k++)
-{
-y = k+3;
-}
-
-//After loop jamming
-for(int k=0;k<10;k++)
-{
-x = k*2;
-y = k+3;
-}
-
-3. Loop Unrolling : 
-
-//Before Loop Unrolling
-
-for(int i=0;i<2;i++)
-{
-printf("Hello");
-}
-
-//After Loop Unrolling
-
-printf("Hello");
-printf("Hello");
+return 0; }
+/*
+Input:
+Enter the Number of Values:5
+left: a
+right: 9
+left: b
+right: c+d
+left: e
+right: c+d
+left: f
+right: b+e
+left: k
+right: 12
+Output:
+Intermediate Code
+a=9
+b=c+d
+e=c+d
+f=b+e
+k=12
+After Dead Code Elimination
+b       =c+d
+ 
+ e       =c+d
+k       =12
+Eliminate Common Expression
+b       =c+d
+b       =c+d
+k       =12
+Optimized Code
+b=c+d
+k=12
+*/
